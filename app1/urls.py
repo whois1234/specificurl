4521b94589eadbe837b0ac00a32a846c44397bff
whois1234 <girish1435337@gmail.com>
2022-03-18 18:24:29 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,4 @@
+from app1.views import *
+from django.urls import path
+app_name='app1'
+urlpatterns=[path('a1_dhoni/',a1_dhoni,name='a1_dhoni'),]
---
+++ app2/urls.py
@@ -0,0 +1,4 @@
+from app2.views import *
+from django.urls import path
+app_name='app2'
+urlpatterns=[path('a2_kohli/',a2_kohli,name='a2_kohli')]
